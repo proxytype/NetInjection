47b77f7034fe2e5c586912dd94ecd4ebd33e688d
proxytype <shlomiruder@gmail.com>
2021-04-17 22:08:52 +0300

    Add files via upload

---
+++ dummy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dummy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+               
+                Console.WriteLine("HI FROM CONSOLE");
+                Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
