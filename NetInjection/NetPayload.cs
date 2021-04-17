5d74d2caede1d325f5a8ec707e142fa0ebe0b437
proxytype <shlomiruder@gmail.com>
2021-04-17 22:12:05 +0300

    Add files via upload

--- NetInjection/NetPayload.cs
+++ NetInjection/NetPayload.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetInjection
 {
-    public class NetPayload
+    class NetPayload
     {
-        public static void showInjection(string caller)
+        static int showInjection(String caller)
         {
+            while (true)
+            {
+                Console.WriteLine("HI FROM INJECTION");
+                Thread.Sleep(1000);
+            }
 
-            System.Windows.Forms.MessageBox.Show("This Is .Net Injection Function!");
+            return 1;
         }
-
-
     }
 }
